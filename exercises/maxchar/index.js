e54629da6b555aed08bc1847e19d0e246632159a
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-03-30 14:02:13 -0400

    Max character solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const characterMap = {};
+  let max = 0;
+  let maxCharacter = '';
+
+  for (let character of str) {
+    if (characterMap[character]) {
+      characterMap[character]++;
+    } else {
+      characterMap[character] = 1;
+    }
+  }
+
+  for (let character in characterMap) {
+    if (characterMap[character] > max) {
+      max = characterMap[character];
+      maxCharacter = character;
+    }
+  }
+
+  return maxCharacter;
+}
 
 module.exports = maxChar;
